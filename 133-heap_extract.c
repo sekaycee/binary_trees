eb10ce5130d3e003aa018ea9d7d36ed0373564ee
Kay Cee <se.kaycee@gmail.com>
2023-03-02 12:50:09 +0100

    edit 133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,99 +1,47 @@
 #include "binary_trees.h"
 
-void heapify_down(heap_t *root)
-{
-	int tmp;
-	heap_t *current = root;
-
-	/* Percolate down the new root node */
-	while (current->left || current->right)
-	{
-		if (!current->left)
-		{
-	    		if (current->n < current->right->n)
-			{
-				tmp = current->n;
-				current->n = current->right->n;
-				current->right->n = tmp;
-				current = current->right;
-			}
-			break;
-		}
-		else if (!current->right)
-		{
-			if (current->n < current->left->n) {
-				tmp = current->n;
-				current->n = current->left->n;
-				current->left->n = tmp;
-				current = current->left;
-	    		}
-			break;
-	    	}
-		else if (current->left->n > current->right->n)
-		{
-	    		if (current->n < current->left->n)
-			{
-				tmp = current->n;
-				current->n = current->left->n;
-				current->left->n = tmp;
-				current = current->left;
-			}
-			break;
-	    	}
-		else
-		{
-	    		if (current->n < current->right->n)
-	    		{
-				tmp = current->n;
-				current->n = current->right->n;
-				current->right->n = tmp;
-				current = current->right;
-			}
-			break;
-		}
-	}
-}
-
+/**
+ * heap_extract - extracts the root node from a Max Binary Heap.
+ * @root: A pointer to the heap root.
+ *
+ * Return: The value of extracted node - if success
+ *         0 - on failure
+ **/
 int heap_extract(heap_t **root)
 {
-	int root_value;
-	heap_t *max_node = *root;
+	heap_t *node;
+	int max_element;
 
 	if (!*root)
 		return (0);
 
-	/* If the root node is a leaf, just delete it */
-	root_value = (*root)->n;
-	if (!(*root)->left && !(*root)->right)
-	{
-		free(*root);
-		*root = NULL;
-		return (root_value);
-	}
+	/* Extract the maximum element (i.e., the root) */
+	max_element = (*root)->n;
 
-	/* Find the node with the maximum value */
-	while (max_node->left || max_node->right)
+	/* Find the node to remove (i.e., the rightmost leaf) */
+	*node = *root;
+	while (node->left || node->right)
 	{
-		if (!max_node->left)
-			max_node = max_node->right;
-		else if (!max_node->right)
-			max_node = max_node->left;
-		else if (max_node->left->n > max_node->right->n)
-			max_node = max_node->left;
+		if (!node->right || node->left->n >= node->right->n)
+	    		node = node->left;
 		else
-			max_node = max_node->right;
+	    		node = node->right;
 	}
 
-	/* Swap the maximum node with the root node */
-	(*root)->n = max_node->n;
-	if (max_node->parent != *root)
-		max_node->parent->right = NULL;
+	/* Replace the root with the node to remove */
+	(*root)->n = node->n;
+	if (node->parent)
+	{
+		if (node == node->parent->left)
+			node->parent->left = NULL;
+		else
+			node->parent->right = NULL;
+	}
 	else
-		(*root)->left = NULL;
+		*root = NULL;
 
-	free(max_node);
-	heapify_down(&(*root));
+	/* Free the memory used by the node to remove */
+	free(node);
 
-	return (root_value);
+	return (max_element);
 }
-
